31afbfe2729c45286aa4677064f172e8aaf1356c
alya783 <76521831+alya783@users.noreply.github.com>
2021-08-13 13:33:53 +0300

    Update index.js
    write a function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let reverseNum = '';
+	while(n > 0){
+		let oneNum = n % 10;
+		reverseNum += String(oneNum);
+		n = Math.floor(n / 10);
+	}
+	return +reverseNum;	
 }
